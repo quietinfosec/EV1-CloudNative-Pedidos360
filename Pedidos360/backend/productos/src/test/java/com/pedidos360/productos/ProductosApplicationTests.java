f10d5f05581d4c2770cd16b6eab1fe05d1cd27d7
Benjamin Rojas <benj.rojasp@duocuc.cl>
2026-08-30 19:55:47 -0400

    Configuración inicial de microservicios y gitignore

---
+++ Pedidos360/backend/productos/src/test/java/com/pedidos360/productos/ProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedidos360.productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
